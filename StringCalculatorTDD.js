4f9b1d258669f485b15c9b8c17c866173a1f674e
MohitMehto98 <mohitmahto98@gmail.com>
2024-07-20 02:45:40 +0530

    implement second test case which is for one number

--- StringCalculatorTDD.js
+++ StringCalculatorTDD.js
@@ -2,7 +2,8 @@ class StringCalculatorTDD {
 	add(numbers) {
 		if (numbers === "") {
 			return 0;
-		}
+        }
+        return parseInt(numbers, 10)
 	}
 }
 
--- StringCalculatorTDD.test.js
+++ StringCalculatorTDD.test.js
@@ -10,5 +10,11 @@ describe("StringCalculatorTDD", () => {
 
 	test("this test case should return 0 for an empty string", () => {
 		expect(calculator.add("")).toBe(0);
-	});
+    });
+    
+    //  this is the test case in which return number for single numner
+    test("this test case should return the number for a single number", () => {
+        expect(calculator.add("1")).toBe(1);
+        expect(calculator.add("2")).toBe(2);
+      });
 });
