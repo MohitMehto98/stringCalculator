4a9f32df9509fa45aeb133aaaf0daca9776a4519
MohitMehto98 <mohitmahto98@gmail.com>
2024-07-20 03:10:40 +0530

    implement test case which is for custom delimiters

--- StringCalculatorTDD.js
+++ StringCalculatorTDD.js
@@ -2,10 +2,24 @@ class StringCalculatorTDD {
 	add(numbers) {
 		if (numbers === "") {
 			return 0;
-        }
-        const numberArray = numbers.split(/,|\n/);
-        return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
+		}
+
+		let delimiter = /,|\n/;
+
+		if (numbers.startsWith("//")) {
+			const delimiterLastIndex = numbers.indexOf("\n");
+			const delimiterPart = numbers.substring(2, delimiterLastIndex);
+			delimiter = new RegExp(ecscapeRegExpression(delimiterPart), "g");
+			numbers = numbers.substring(delimiterLastIndex + 1);
+		}
+
+		const numberArray = numbers.split(delimiter);
+		return numberArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 	}
+}
+
+function ecscapeRegExpression(string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 module.exports = StringCalculatorTDD;
--- StringCalculatorTDD.test.js
+++ StringCalculatorTDD.test.js
@@ -6,36 +6,41 @@ describe("StringCalculatorTDD", () => {
 		calculator = new StringCalculatorTDD();
 	});
 
-    // this is the test case for empty string
-    
+	// this is the test case for empty string
+
 	test("this test case should return 0 for an empty string", () => {
 		expect(calculator.add("")).toBe(0);
-    });
-    
-    //  this is the test case for single numner
-    test("this test case should return the number for a single number", () => {
-        expect(calculator.add("1")).toBe(1);
-        expect(calculator.add("2")).toBe(2);
-      });
-    
-    //  thisis the test case for two numbers
-    test("should return the sum of two numbers", () => {
-          expect(calculator.add("1,2")).toBe(3);
-          expect(calculator.add("3,4")).toBe(7);
-    });
-    
-    //  this is the test case for Unknown no. of Numbers
-    test("should return the sum of an unknown number of numbers", () => {
-        expect(calculator.add("1,2,3")).toBe(6);
-        expect(calculator.add("4,5,6,7,8,9")).toBe(39);
-    });
+	});
 
-    // this is the test case for new line btw numbers
-    
-    test("should handle new lines between numbers", () => {
-        expect(calculator.add("1\n2,3")).toBe(6);
-        expect(calculator.add("4\n5\n6")).toBe(15);
-      });
+	//  this is the test case for single numner
+	test("this test case should return the number for a single number", () => {
+		expect(calculator.add("1")).toBe(1);
+		expect(calculator.add("2")).toBe(2);
+	});
+
+	//  thisis the test case for two numbers
+	test("should return the sum of two numbers", () => {
+		expect(calculator.add("1,2")).toBe(3);
+		expect(calculator.add("3,4")).toBe(7);
+	});
+
+	//  this is the test case for Unknown no. of Numbers
+	test("should return the sum of an unknown number of numbers", () => {
+		expect(calculator.add("1,2,3")).toBe(6);
+		expect(calculator.add("4,5,6,7,8,9")).toBe(39);
+	});
+
+	// this is the test case for new line btw numbers
+
+	test("should handle new lines between numbers", () => {
+		expect(calculator.add("1\n2,3")).toBe(6);
+		expect(calculator.add("4\n5\n6")).toBe(15);
+	});
+
+	// this is the test case for support custorm delimiters
+
+	test("should handle custom delimiters", () => {
+		expect(calculator.add("//;\n1;2")).toBe(3);
+		expect(calculator.add("//|\n1|2|3")).toBe(6);
+	});
 });
-
-  
